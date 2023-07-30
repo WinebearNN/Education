172707841bb7f3970f49b368d6fe5f8e3fba2b47
Vova Zimin <ziminvova44@gmail.com>
2023-07-30 12:06:17 +0300

    upload: new theory about Date

---
+++ src/theory/article/ClassDate.java
@@ -0,0 +1,16 @@
+package theory.article;
+
+public class ClassDate {
+//    Date() - создает объект Date для текущего времени
+//    Date(elapseTime:long) - создает объект Date для заданного времени в миллисекундах, прошедшего с 01.01.1970
+//    toString():String - возвращает строку, представляющуу дату и время
+//    getTime():long - возвращает кол-во миллисекунд, прошедших с 01.01.1970
+//    setTime(elapseTime:long):void - устанавливает новое время
+
+
+
+
+
+
+
+}
