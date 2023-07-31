fd580801fab6f6fbd8db2f2d70d70e03fa47ec0f
Vova Zimin <ziminvova44@gmail.com>
2023-08-01 00:26:48 +0300

    update: new theory about class Date

--- src/theory/article/ClassDate.java
+++ src/theory/article/ClassDate.java
@@ -1,16 +1,21 @@
 package theory.article;
 
+import java.util.Date;
+
 public class ClassDate {
-//    Date() - создает объект Date для текущего времени
+    //    Date() - создает объект Date для текущего времени
 //    Date(elapseTime:long) - создает объект Date для заданного времени в миллисекундах, прошедшего с 01.01.1970
 //    toString():String - возвращает строку, представляющуу дату и время
 //    getTime():long - возвращает кол-во миллисекунд, прошедших с 01.01.1970
 //    setTime(elapseTime:long):void - устанавливает новое время
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.getTime());
+        Date date2 = new Date(1690833983555L);
+        System.out.println(date2.toString());
+        System.out.println(date2.getTime());
 
-
-
-
-
+    }
 
 
 }
