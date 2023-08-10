744d1965995b4178b647f8cc82fa5d91d5978d0b
Vova Zimin <ziminvova44@gmail.com>
2023-08-10 12:30:09 +0300

    i don't know

--- src/myDevelopment/testing/ZZTesting.java
+++ src/myDevelopment/testing/ZZTesting.java
@@ -13,12 +13,12 @@ public class ZZTesting {
 }
 
 
-class TestDeleteWhiteSpacesFromStrings{
+class TestDeleteWhiteSpacesFromStrings {
     public static void main(String[] args) {
 
         double[][] matrix = new double[2][];
-        matrix[0]=new double[] {1,2,3,4,5,6,7,8,9,10};
-        matrix[1] = new double[] {11,12,13,14,15,16,17,18,19,20};
+        matrix[0] = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        matrix[1] = new double[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
         //System.out.println(Arrays.toString(array));
         //System.out.println(Factorial.FactorialInteger(5));
@@ -28,5 +28,6 @@ class TestDeleteWhiteSpacesFromStrings{
         System.out.println(Arrays.toString(FindPrimeNumbersInArrayOrMatrix.FindPrimeNumbersMatrix(matrix)));
     }
 }
+
 
 
