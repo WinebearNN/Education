c67a39ebe0f2d74192da514ee8a4c5d90a3b8486
Vova Zimin <ziminvova44@gmail.com>
2023-08-14 01:40:01 +0300

    fix: remake

--- src/xRep/homework/Account.java
+++ src/xRep/homework/Account.java
@@ -1,137 +1,106 @@
 package xRep.homework;
 
-import java.util.Date;
 import java.util.Scanner;
 
 public class Account {
-    private double balance = 10000;
-    private double annualInterestRate = 0;
-
-    private final int monthsPerYear = 12;
-
-    private Date dateCreate = new Date();
-
-
-    public Date getDateCreate() {
-        return dateCreate;
-    }
-
-    public double getAnnualInterestRate() {
-        return annualInterestRate;
-    }
-
-    public void setAnnualInterestRate(double annualInterestRate) {
-        this.annualInterestRate = annualInterestRate;
-    }
+    private Account[] accounts = new Account[10];
+    private double balance;
 
     public double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
+    private int id;
+    private static Scanner input = new Scanner(System.in);
 
     public Account() {
-
+        for (int i = 0; i < 10; i++) {
+            accounts[i] = new Account(10000, i);
+        }
     }
 
-
-    public double getMonthlyInterest() {
-        return (((this.annualInterestRate / this.monthsPerYear) / 100) * this.balance);
+    public Account(double balance, int id) {
+        this.balance = balance;
+        this.id = id;
     }
 
-    public double withdraw(double money) {
-        this.balance -= money;
-        return this.balance;
-    }
-
-    public double deposit(double money) {
-        this.balance += money;
-        return this.balance;
-    }
-
-    private int tempId;
-    private int[] id = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-
-    public Account(int id) {
-        this.tempId = id;
-    }
-
-    public boolean checkId() {
-        int count = 0;
-        for (int i : this.id) {
-            if (this.tempId == i) {
-                count += 1;
+    public int getMenu() {
+        int choice;
+        while (true) {
+            System.out.println("основное меню");
+            System.out.println("1)показать баланс");
+            System.out.println("2)снять деньгу");
+            System.out.println("3)положить деньга");
+            System.out.println("4)выйти");
+            System.out.println("введите пункт меню");
+            choice = input.nextInt();
+            if (choice < 1 || choice > 4) {
+                System.out.println("иди нахуй");
             } else {
+                break;
+            }
+        }
+        return choice;
+    }
+
+    public void withdraw(int id,double money) {
+        accounts[id].balance -= money;
+    }
+
+    public void deposit(double money) {
+        this.balance += money;
+    }
+
+    public void withdraw(int id) {
+        System.out.println("введите сумму для снятия");
+        double money = input.nextDouble();
+        if (money < 0) {
+            System.out.println("сумма отрицательна");
+        } else if (money <= accounts[id].getBalance()) {
+            accounts[id].withdraw(id,money);
+        } else {
+            System.out.println("денег нема");
+        }
+    }
+
+    public void deposit(int id) {
+        System.out.println();
+        double money = input.nextDouble();
+        if (money >= 0) {
+            accounts[id].deposit(money);
+        }else{
+            System.out.println("операция отклонена: сумма отрицательнеа");
+        }
+    }
+
+    public static void main(String[] args) {
+        Account accountATM = new Account();
+        int id;
+        while (true) {
+            System.out.println("введи id");
+            id = input.nextInt();
+            if(id<0 || id>9){
+                System.out.println("еблан");
                 continue;
             }
-        }
-        if (count == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-}
-
-//class TestAccount {
-//    public static void main(String[] args) {
-//        Account user = new Account(1112, 20000);
-//        user.setAnnualInterestRate(4.5);
-//        user.withdraw(2500);
-//        user.deposit(3000);
-//        System.out.println(user.getBalance());
-//        System.out.println(user.getDateCreate());
-//        System.out.println(user.getMonthlyInterest());
-//    }
-//}
-class TestAccount {
-    public static void main(String[] args) {
-        for (int j = 0; j < 999; j++) {
-            for (int i = 0; i < 999; i++) {
-                System.out.println("Введите id");
-                Scanner s = new Scanner(System.in);
-                int id = s.nextInt();
-                Account acc = new Account(id);
-                if (!acc.checkId()) {
-                    System.out.println("неправильно набран id");
-                } else {
-                    System.out.println("Введите 1 для просмотра баланса");
-                    System.out.println("Введите 2 для снятия денег со счета");
-                    System.out.println("Введите 3 для внесения денег на счет");
-                    System.out.println("Введите 4 для выхода из основного меню");
-                    int step = s.nextInt();
-                    if (step == 1) {
-                        System.out.println(acc.getBalance());
-                        break;
-                    }
-                    if (step == 2) {
-                        System.out.println("Введите сумму для снятия");
-                        int money = s.nextInt();
-                        acc.withdraw(money);
-                        System.out.println("Баланс");
-                        System.out.println(acc.getBalance());
-                        break;
-                    }
-                    if (step == 3) {
-                        System.out.println("Введите сумму для пополнения");
-                        int money = s.nextInt();
-                        acc.deposit(money);
-                        System.out.println("Баланс");
-                        System.out.println(acc.getBalance());
-                        break;
-                    }
-                    if (step == 4) {
-                        break;
-                    }
-                    i--;
-                    j--;
+            while (true){
+                int choice =accountATM.getMenu();
+                if (choice==1){
+                    System.out.println("баланс равен " + accountATM.accounts[id].getBalance());
+                }else if(choice==2){
+                    accountATM.accounts[id].withdraw(id);
+                } else if (choice==3) {
+                    accountATM.accounts[id].deposit(id);
+                } else if (choice==4) {
+                    break;
                 }
+
             }
         }
+
+
     }
 }
+
+
+
