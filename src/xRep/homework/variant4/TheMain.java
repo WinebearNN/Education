c2b9d98c7e45f784018460582925d7cc4e7ad90b
Vova Zimin <ziminvova44@gmail.com>
2023-08-30 13:44:23 +0300

    fix: last hw

--- src/xRep/homework/variant4/TheMain.java
+++
@@ -1,7 +0,0 @@
-package xRep.homework.variant4;
-
-class TheMain extends Manager {
-    public TheMain(String name, String secName, String lastName, int age, Character character, Position position, Priority priority) {
-        super(name, secName, lastName, age, character, position,priority);
-    }
-}
